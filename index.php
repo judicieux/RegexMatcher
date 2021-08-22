41c8fcdc3eb3cb1242d8d3b597a624894f8d8134
g3zb0yy <r4nd0mmachine@gmail.com>
2021-08-22 15:55:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-type" content="text/html" charset="utf-8"/>
+	<meta http-equiv="Content-type"content="text/html; charset=utf-8"/>
 	<link rel="stylesheet" href="css/style.css"/>
 </head>
 <center>
